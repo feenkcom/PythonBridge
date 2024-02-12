feef2c2db697a61df57c925709d430686735443e
svenvc <sven@stfx.eu>
2024-02-12 15:09:51 +0100

    updated publish_gtoolkit_bridge_PyPI.sh

--- scripts/publish_gtoolkit_bridge_PyPI.sh
+++ scripts/publish_gtoolkit_bridge_PyPI.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "publish gtoolkit_bridge PyPI"
+echo "running publish gtoolkit_bridge PyPI script"
 
 # requirements: 
 # - python3 (apt install python)
@@ -8,14 +8,17 @@ echo "publish gtoolkit_bridge PyPI"
 # - jq (apt install jq)
 # - flit (pip install flit)
 
-source_version=`cd PyPI/src && python3 -c 'from gtoolkit_bridge import __version__; print(__version__)'`
+# this script must be run inside the PyPI directory
+
+source_version=`cd src && python3 -c 'from gtoolkit_bridge import __version__; print(__version__)'`
 published_version=`curl -s https://pypi.org/pypi/gtoolkit_bridge/json | jq -r .info.version`
 
 if [ $source_version = $published_version ]
 then
   echo "no action" $source_version "=" $published_version
 else
-  echo "publish" $published_version ">" $source_version
+  echo "publishing" $published_version ">" $source_version
   # FLIT_PASSWORD must already be set to token pypi-...
-  # FLIT_USERNAME=__token__ python3 -m flit publish
+  FLIT_USERNAME=__token__ python3 -m flit publish
+  echo "publish done"
 fi
