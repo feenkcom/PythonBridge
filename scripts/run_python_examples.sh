aabb60f4514fcb953a5cb00e75a7931841730ba0
svenvc <sven@stfx.eu>
2024-02-20 16:50:11 +0100

    debugging run_python_examples.sh

--- scripts/run_python_examples.sh
+++ scripts/run_python_examples.sh
@@ -2,6 +2,13 @@
 
 echo "Running Python Example"
 
+image_dir = `pwd`
+
+cd ..
+
 export PATH=/home/ubuntu/.local/bin:$PATH
-xvfb-run -a ./gt-installer --verbose --workspace ${GlamorousToolkit.EXAMPLES_FOLDER} test --disable-deprecation-rewrites --packages 'PythonBridge' 'PythonBridge-Pharo' 
-xvfb-run -a ./gt-installer --verbose --workspace ${GlamorousToolkit.EXAMPLES_FOLDER} test 'PythonBridge-Pharo'
+
+xvfb-run -a ./gt-installer --verbose --workspace ${image_dir} test --disable-deprecation-rewrites --packages 'PythonBridge' 'PythonBridge-Pharo' 
+
+# xvfb-run -a ./gt-installer --verbose --workspace ${image_dir} test 'PythonBridge-Pharo'
+
