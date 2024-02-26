05aa9f768d73432e5be828a63debce1d7b8f768b
svenvc <sven@stfx.eu>
2024-02-26 19:14:39 +0100

    simplified version of run_python_examples.sh that was manually tested on scooby-doo

--- scripts/run_python_examples.sh
+++ scripts/run_python_examples.sh
@@ -10,7 +10,4 @@ export PATH=/home/ubuntu/.local/bin:$PATH
 
 pipenv --version
 
-xvfb-run -a ./gt-installer --verbose --workspace ${image_dir} test --disable-deprecation-rewrites --packages 'PythonBridge' 'PythonBridge-Pharo' 
-
-# xvfb-run -a ./gt-installer --verbose --workspace ${image_dir} test 'PythonBridge-Pharo'
-
+./gt-installer --verbose --workspace ${image_dir} test --disable-deprecation-rewrites --packages 'PythonBridge' 'PythonBridge-Pharo'
