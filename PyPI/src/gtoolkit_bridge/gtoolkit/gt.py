87c7f331531202fda48cda7897a8d7d01eefaaa8
Sven Van Caekenberghe <sven@beta9.be>
2024-07-03 14:06:41 +0200

    Improve PythonBridge robustness inspecting special objects (https://github.com/feenkcom/gtoolkit/issues/3858)
    Use newly added getattrsafe function to guard against errors
    Add explicit str() conversion in value column item formatter of attributes and methods views

--- PyPI/src/gtoolkit_bridge/gtoolkit/gt.py
+++ PyPI/src/gtoolkit_bridge/gtoolkit/gt.py
@@ -6,6 +6,12 @@ def gtView(func):
     setattr(func, "gtView", True)
     return func
 
+def getattrsafe(object, name, default):
+    try:
+        return getattr(object, name, default)
+    except:
+        return default
+
 
 class GtViewedObject:
     def __init__(self, obj):
@@ -17,16 +23,20 @@ class GtViewedObject:
     def getGtViewMethodNames(self):
         # those defined by me
         allMyAttributes = dir(self)
-        allMyMethods = filter(lambda each: callable(getattr(self, each, None)), allMyAttributes)
-        myGtViews = filter(lambda each: getattr(getattr(self, each, None), "gtView", False), allMyMethods)
+        allMyMethods = filter(lambda each: callable(getattrsafe(self, each, None)),
+                              allMyAttributes)
+        myGtViews = filter(lambda each: getattrsafe(getattrsafe(self, each, None), "gtView", False),
+                           allMyMethods)
         # those defined by the object that I wrap
         allObjectAttributes = dir(self.object)
-        allObjectMethods = list(filter(lambda each: callable(getattr(self.object, each, None)), allObjectAttributes))
-        objectGtViews = filter(lambda each: getattr(getattr(self.object, each, None), "gtView", False),
+        allObjectMethods = list(filter(lambda each: callable(getattrsafe(self.object, each, None)),
+                                       allObjectAttributes))
+        objectGtViews = filter(lambda each: getattrsafe(getattrsafe(self.object, each, None), "gtView", False),
                                allObjectMethods)
         # for now, we keep the old prefix matching, avoiding doubles
         objectGtViews = set(objectGtViews)
-        objectGtViews.update(filter(lambda each: each.startswith("gtView"), allObjectMethods))
+        objectGtViews.update(filter(lambda each: each.startswith("gtView"),
+                                    allObjectMethods))
         # combined into a list of strings
         result = list(set(list(myGtViews) + list(objectGtViews)))
         # when I wrap a @gtView marked method I get false positives,
@@ -40,8 +50,8 @@ class GtViewedObject:
         return result
 
     def getView(self, viewName):
-        myView = getattr(self, viewName, None)
-        objectView = getattr(self.object, viewName, None)
+        myView = getattrsafe(self, viewName, None)
+        objectView = getattrsafe(self.object, viewName, None)
         if objectView is not None:
             return objectView(ViewBuilder())
         if myView is not None:
@@ -70,8 +80,10 @@ class GtViewedObject:
 
     def attributesFor(self, anObject, callables):
         allAttributes = dir(anObject)
-        filteredAttributes = filter(lambda each: callable(getattr(anObject, each, None)) == callables, allAttributes)
-        keyValuePairs = map(lambda each: [each, getattr(anObject, each, "")], filteredAttributes)
+        filteredAttributes = filter(lambda each: callable(getattrsafe(anObject, each, None)) == callables,
+                                    allAttributes)
+        keyValuePairs = map(lambda each: [each, getattrsafe(anObject, each, "")],
+                            filteredAttributes)
         return list(keyValuePairs)
 
 
@@ -82,7 +94,7 @@ class GtViewedObject:
             .priority(200) \
             .items(lambda: self.attributesFor(self.object, False)) \
             .column("Name", lambda each: each[0]) \
-            .column("Value", lambda each: each[1]) \
+            .column("Value", lambda each: str(each[1])) \
             .set_accessor(lambda selection: self.attributesFor(self.object, False)[selection][1])
 
     @gtView
@@ -92,7 +104,7 @@ class GtViewedObject:
             .priority(250) \
             .items(lambda: self.attributesFor(self.object, True)) \
             .column("Name", lambda each: each[0]) \
-            .column("Value", lambda each: each[1]) \
+            .column("Value", lambda each: str(each[1])) \
             .set_accessor(lambda selection: self.attributesFor(self.object, True)[selection][1])
 
     @gtView
